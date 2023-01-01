2bf80298c01057b05cd1cad15925b79eb7796e36
jolle <jolle@users.noreply.github.com>
2023-01-01 17:41:04 +0200

    🏗️ Move to GHA from Travis

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    roots: ['<rootDir>'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
